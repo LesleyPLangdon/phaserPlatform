69e41748b95811f1d37f7140bf55be88e65c68e7
Lesley Potts-Langdon <61936741+LesleyPLangdon@users.noreply.github.com>
2024-11-27 17:21:04 +0000

    changed Background

--- game.js
+++ game.js
@@ -21,7 +21,7 @@ var game = new Phaser.Game(config);
 
 function preload() {
     // This is where we'll load our game assets
-    this.load.image('background', 'spaceBackground.png');
+    this.load.image('background', 'spaceBackground2.png');
     this.load.image('ground', 'assets/grass.png');
     this.load.image('player', 'assets/alienGreen_walk1.png');
     this.load.image('box', 'assets/boxCoin_disabled.png');
@@ -46,7 +46,7 @@ function create() {
    // Set world bounds
     this.physics.world.setBounds(0, 0, 4000, 2000);  // The second parameter is the world height
     background = this.add.image(0, 0, 'background');
-    background.setScale(4, 2);
+    background.setScale(2, 2);
 
     platforms = this.physics.add.staticGroup();  // Create a group for platforms
     
